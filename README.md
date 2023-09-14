b6e8893281de853ea178acce8a420e19a5599244
alinealinealine <xweng@ucdavis.edu>
2023-09-14 02:23:54 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ README.md
@@ -0,0 +1,1 @@
+# Development Impact GPT
---
+++ components/Footer.tsx
@@ -0,0 +1,52 @@
+import { IconBrandGithub, IconBrandTwitter } from "@tabler/icons-react";
+import { FC } from "react";
+
+export const Footer: FC = () => {
+  return (
+    <div className="flex h-[50px] border-t border-gray-300 py-2 px-8 items-center sm:justify-between justify-center">
+      <div className="hidden sm:flex"></div>
+
+      <div className="hidden sm:flex italic text-sm">
+        Inspired by Paul Graham GPT by
+        <a
+          className="hover:opacity-50 mx-1"
+          href="https://twitter.com/mckaywrigley"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Mckay Wrigley.
+        </a>
+        Created by
+        <a
+          className="hover:opacity-50 ml-1"
+          href="https://twitter.com/gauravrpjain"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Gaurav Jain 
+        </a>
+         and Aline Weng.
+      </div>
+
+      <div className="flex space-x-4">
+        <a
+          className="flex items-center hover:opacity-50"
+          href="https://twitter.com/gauravrpjain"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <IconBrandTwitter size={24} />
+        </a>
+
+        <a
+          className="flex items-center hover:opacity-50"
+          href="https://github.com/gauravrpjain/dev-impact-gpt"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <IconBrandGithub size={24} />
+        </a>
+      </div>
+    </div>
+  );
+};
